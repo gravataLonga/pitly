2496eb9198987c68d1404c88522872bd4a357802
Jonathan Fontes <jonathan.alexey16@gmail.com>
2017-12-17 18:51:44 +0000

    CHG: Vue Components for the Shorten UI Enviroments

---
+++ resources/assets/js/components/ShortenComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="flex justify-center">
+            <div class="w-1/2">
+                <p class="text-center mb-8 text-grey-darkest text-base font-sans">Cria o teu primeiro URL cortado!</p>
+                <input name="url" type="text" class="w-full px-2 py-2 text-2xl text-grey-darker rounded shadow" v-model="url">
+            </div>
+        </div>
+        <div class="flex justify-center mt-4">
+            <div class="w-1/4 text-center">
+                <button @click="create" type="submit" class="hover:text-pitly-dark hover:bg-white rounded text-white shadow px-2 py-2 bg-pitly-dark text-base font-sans">Corta</button>
+            </div>
+        </div>
+        <div class="flex mt-8">
+            <div class="w-1/4 rounded shadow bg-white border-2 border-pitly-dark px-2 py-4" v-for="item in shortens">
+                <h4><a class="text-pitly-dark font-sans" v-bind:href="item.shorted">{{ item.token }}</a></h4>
+                <p class="mt-2 color-sm text-grey font-sans">{{item.original}}</p>
+            </div>
+        </div>
+    </div>  
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                url: '',
+                shortens: []
+            }
+        },
+        methods: {
+            create() {
+                axios.post('shorter', {
+                    url: this.url
+                }).then(r => {
+                    this.shortens.push(r.data.data);
+                    this.url = ''
+                });
+            }
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,32 +4,20 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Pitly</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class="bg-pitly">
-        <div class="container mx-auto">
+        <div class="container mx-auto" id="shorten-app">
             <div class="flex justify-center">
                 <div class="w-1/4 py-8">
                     <img src="{{ asset('images/pitly.png') }}" alt="Pitly">
                 </div>
             </div>
-            <!-- This can be done via Vue.js with axios -->
-            <form action="{{ route('shorter.store') }}" method="POST">
-                {{ csrf_field() }}
-                <div class="flex justify-center">
-                    <div class="w-1/2">
-                        <p class="text-center mb-8 text-grey-darkest text-base font-sans">Cria o teu primeiro URL cortado!</p>
-                        <input name="url" type="text" class="w-full px-2 py-2 text-2xl text-grey-darker rounded shadow">
-                    </div>
-                </div>
-                <div class="flex justify-center mt-8">
-                    <div class="w-1/4 text-center">
-                        <button type="submit" class="hover:text-pitly-dark hover:bg-white rounded text-white shadow px-2 py-2 bg-pitly-dark text-base font-sans">Corta</button>
-                    </div>
-                </div>
-            </form>
+            <shorten-component></shorten-component>
         </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
